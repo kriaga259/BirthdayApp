94db750e07f43829d7d66b7ef5816e811f488344
Krisha <akrisha259@gmail.com>
2025-07-25 10:31:46 -0700

    added friend class and UI to display list and create new friend entry

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "goldfish", birthday: .now),
+        Friend(name: "whale", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List (friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) { //pin the new friend-entry UI below list of friends
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
-        .padding()
+//        .padding()
     }
 }
 
